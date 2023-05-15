0fc415aa4b878a878ff922a859a628bb00eb8a02
Rogério Ricardo Nunes <rogerioricardonunes@gmail.com>
2023-05-15 20:12:16 -0300

    Retirando cors

--- src/main/java/br/com/softnunes/clienteapi/ClienteApiApplication.java
+++ src/main/java/br/com/softnunes/clienteapi/ClienteApiApplication.java
@@ -1,12 +1,7 @@
 package br.com.softnunes.clienteapi;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class ClienteApiApplication {
@@ -14,13 +9,4 @@ public class ClienteApiApplication {
 		SpringApplication.run(ClienteApiApplication.class, args);
 	}
 
-	@Bean
-	public CorsFilter corsFilter() {
-		CorsConfiguration corsConfiguration = new CorsConfiguration();
-		corsConfiguration.setAllowedOrigins(Arrays.asList("https://cliente-ui.herokuapp.com"));
-		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-		return new CorsFilter(urlBasedCorsConfigurationSource);
-	}
 }
