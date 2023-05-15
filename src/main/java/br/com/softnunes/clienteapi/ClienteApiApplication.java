a75b3d815d9e1d7336f1d71e8ef8788ebf12d508
Rogério Ricardo Nunes <rogerioricardonunes@gmail.com>
2023-05-15 20:01:31 -0300

    Add config cors do heroku

--- src/main/java/br/com/softnunes/clienteapi/ClienteApiApplication.java
+++ src/main/java/br/com/softnunes/clienteapi/ClienteApiApplication.java
@@ -21,7 +21,7 @@ public class ClienteApiApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("https://cliente-ui.herokuapp.com/"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://cliente-ui.herokuapp.com"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", ACCESS_CONTROL_ALLOW_ORIGIN, "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
